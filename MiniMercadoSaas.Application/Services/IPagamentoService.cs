d78a947cbffd902269ae85e64bc3300483fe8826
Arthur Victor <3emebeats@gmail.com>
2026-05-23 11:24:10 -0300

    Delete MiniMercadoSaas.Application/Services/IPagamentoService.cs

--- MiniMercadoSaas.Application/Services/IPagamentoService.cs
+++
@@ -1,8 +0,0 @@
-using MiniMercadoSaas.Application.DTO.Response;
-
-namespace MiniMercadoSaas.Application.Services;
-
-public interface IPagamentoService
-{
-    Task<PixResponse> GerarPagamentoPix(Guid vendaId, decimal valor);
-}
